462d5e885e07613c14eedfa806a746fdcac4c616
jihenJN <jihenjihen23@gmail.com>
2023-11-14 12:01:53 +0100

    freecodecamp javascript

--- src/App.java
+++ src/App.java
@@ -4,41 +4,25 @@ import javax.swing.plaf.basic.BasicInternalFrameTitlePane.SystemMenuBar;
 
 public class App {
     public static void main(String[] args) throws Exception {
-
         Scanner input = new Scanner(System.in);
-        int x = input.nextInt();
-        System.out.println(ToBinary(x));
-        
+        String EOF = "bye";
+        boolean flag = false;
+        int n = 0;
+        while (flag != true) {
 
-    }
+            String line = input.nextLine();
 
-    public static String ToBinary(int x) {
+            n++;
 
-        String binary = "";
-        int i = 0;
-        int r = 0;
+            if (line.compareTo(EOF) == 0) {
+                flag = true;
+            }
 
-        while (x != 0) {
-            r = x % 2;
-            x = x / 2;
-            binary = binary + r;
-            i++;
-            System.out.println(binary);
+           
         }
 
-        return Reverse(binary);
+
+          
+
     }
-
-    public static String Reverse(String ch) {
-        String reverse = "";
-
-        for (int i = 1; i <= ch.length(); i++) {
-
-            reverse = reverse + ch.charAt(ch.length() - i);
-
-        }
-
-        return reverse;
-    }
-
 }
